2088a2897a5c7b032bd9a3773bd0bffc374db6a1
Adebayo Muis <charmingdc002@gmail.com>
2025-01-12 01:26:52 +0100

    Build and prepare for publishing

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/scroll.js',
+      name: 'ScrollJs',
+      fileName: (format) => `scrolljs.${format}.js`,
+      formats: ['es', 'cjs'], 
+    },
+    rollupOptions: {
+      external: [], 
+      output: {
+        globals: {
+        },
+      },
+    },
+  },
+}); 
